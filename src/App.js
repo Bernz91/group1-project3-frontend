125a58d237ad74899ca163246f957ffa6260aa94
Bernz91 <samuel.bernadi91@gmail.com>
2022-10-11 16:16:36 +0800

    added simple login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Component/Home";
 import Fabrics from "./Component/Fabrics";
+import Login from "./Component/Login";
 
 const App = () => {
   console.log("test");
@@ -11,6 +12,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path=":index" element={<Child />} /> */}
         <Route path="/fabrics" element={<Fabrics />} />
       </Routes>
---
+++ src/Component/Login.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+    reset,
+  } = useForm({ mode: "onTouched" });
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    // to put the authO here
+  };
+
+  return (
+    <div className="vertical-center">
+      <Grid2 container columnSpacing={2} rowSpacing={2}>
+        <Grid2 xs={12}>
+          <div className="login-logo">Logo Here</div>
+          <div className="login-subtitle">Do not have an account? Register</div>
+        </Grid2>
+        <Grid2></Grid2>
+      </Grid2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid2 container columnSpacing={2} rowSpacing={2}>
+          {/* Email row */}
+          <Grid2 xs={2} md={3}></Grid2>
+          <Grid2 xs={8} md={6}>
+            <label>Email:</label>
+            <input
+              {...register("email", {
+                required: "⚠ Email Address is required",
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "⚠ Please enter a valid email",
+                },
+              })}
+            />
+            <div className="validation-error">{errors.email?.message}</div>
+          </Grid2>
+          <Grid2 xs={2} md={3}></Grid2>
+
+          {/* third row */}
+          <Grid2 xs={2} md={3}></Grid2>
+          <Grid2 xs={8} md={6}>
+            <label>Password:</label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "⚠ Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters",
+                },
+              })}
+            />
+            <div className="validation-error">{errors.password?.message}</div>
+          </Grid2>
+          <Grid2 xs={2} md={3}></Grid2>
+
+          {/* submit row */}
+          <Grid2 xs={2} md={3}></Grid2>
+          <Grid2 xs={8} md={6}>
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!isDirty || !isValid}
+            >
+              Login
+            </Button>
+          </Grid2>
+          <Grid2 xs={2} md={3}></Grid2>
+
+          {/* Forgot password row */}
+          <Grid2 xs={2} md={3}></Grid2>
+          <Grid2 xs={8} md={6}>
+            <div className="forgot-password">Forgot password</div>
+          </Grid2>
+          <Grid2 xs={2} md={3}></Grid2>
+        </Grid2>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
