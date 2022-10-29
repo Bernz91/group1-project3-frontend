b31736562966f9469246d65a20446998862a33f3
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-10-29 09:57:25 +0800

    customisation page update

--- src/Component/Customisation.js
+++ src/Component/Customisation.js
@@ -10,9 +10,11 @@ import "../CSS/Fabrics.css";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const Customisation = () => {
+  //fabrics
   const [fabrics, setFabrics] = useState([]);
   const [chosenFabric, setChosenFabric] = useState([]);
 
+  //fabrics useEffect
   useEffect(() => {
     axios
       .get(`${BACKEND_URL}/fabrics`)
@@ -20,6 +22,82 @@ const Customisation = () => {
       .then((res) => {
         console.log(res);
         setFabrics(res);
+      });
+  }, []);
+
+  //collars
+  const [collars, setCollars] = useState([]);
+  const [chosenCollar, setChosenCollar] = useState([]);
+
+  //collars useEffect
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/collars`)
+      .then((res) => res.data)
+      .then((res) => {
+        console.log(res);
+        setCollars(res);
+      });
+  }, []);
+
+  //cuffs
+  const [cuffs, setCuffs] = useState([]);
+  const [chosenCuff, setChosenCuff] = useState([]);
+
+  //cuffs useEffect
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/cuffs`)
+      .then((res) => res.data)
+      .then((res) => {
+        console.log(res);
+        setCuffs(res);
+      });
+  }, []);
+
+  //fronts
+  const [fronts, setFronts] = useState([]);
+  const [chosenFront, setChosenFront] = useState([]);
+
+  //fronts useEffect
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/fronts`)
+      .then((res) => res.data)
+      .then((res) => {
+        console.log(res);
+        setFronts(res);
+      });
+  }, []);
+
+  //pockets
+  const [pockets, setPockets] = useState([]);
+  const [chosenPocket, setChosenPocket] = useState([]);
+
+  //pockets useEffect
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/pockets`)
+      .then((res) => res.data)
+      .then((res) => {
+        console.log(res);
+        setPockets(res);
+      });
+  }, []);
+
+  //backs
+  const [backs, setBacks] = useState([]);
+  const [chosenBack, setChosenBack] = useState([]);
+
+  //backs useEffect
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/backs`)
+      .then((res) => res.data)
+      .then((res) => {
+        console.log(res);
+        setBacks(res);
       });
   }, []);
 
@@ -33,7 +111,11 @@ const Customisation = () => {
         <div>Customise your shirt</div>
       </Typography>
       <Typography variant="h5">
-        <div>You have chosen fabric {chosenFabric}.</div>
+        <div>
+          You have chosen fabric {chosenFabric}, collar {chosenCollar}, cuff
+          {chosenCuff}, front {chosenFront}, pocket {chosenPocket}, back{" "}
+          {chosenBack}.
+        </div>
       </Typography>
       <Typography variant="h4" color="white">
         <div>Step One: Fabrics</div>
@@ -54,7 +136,7 @@ const Customisation = () => {
                     {fabric.fabricName}
                   </Typography>
                   <Typography variant="body1" component="div">
-                    Price: ${fabric.cost}
+                    Price: ${fabric.cost} Fabric ID: {fabric.id}
                   </Typography>
                   <Typography variant="body2" fontFamily="default">
                     {fabric.description}
@@ -69,6 +151,233 @@ const Customisation = () => {
                           var getFabricID =
                             event.target.getAttribute("fabricID");
                           setChosenFabric(getFabricID);
+                        }}
+                      >
+                        Select me
+                      </Button>
+                    </CardActions>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </div>
+          </card>
+        );
+      })}
+      <Typography variant="h4" color="white">
+        <div>Step Two: Collars</div>
+      </Typography>
+      {collars.map((collar, index) => {
+        return (
+          <card sx={{ maxWidth: 250 }}>
+            <div key={index}>
+              <ul className="InstaCard">
+                <CardMedia
+                  component="img"
+                  alt="collar"
+                  width="250"
+                  image={collar.imageOne}
+                />
+                <CardContent>
+                  <Typography variant="h5" fontWeight="bold" component="div">
+                    {collar.collarName}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    Price: ${collar.cost} Collar ID: {collar.id}
+                  </Typography>
+                  <Typography variant="body2" fontFamily="default">
+                    {collar.description}
+                  </Typography>
+                  <Typography>
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        collarID={collar.id}
+                        onClick={(event) => {
+                          var getCollarID =
+                            event.target.getAttribute("collarID");
+                          setChosenCollar(getCollarID);
+                        }}
+                      >
+                        Select me
+                      </Button>
+                    </CardActions>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </div>
+          </card>
+        );
+      })}
+      <Typography variant="h4" color="white">
+        <div>Step Three: Cuffs</div>
+      </Typography>
+      {cuffs.map((cuff, index) => {
+        return (
+          <card sx={{ maxWidth: 250 }}>
+            <div key={index}>
+              <ul className="InstaCard">
+                <CardMedia
+                  component="img"
+                  alt="cuff"
+                  width="250"
+                  image={cuff.imageOne}
+                />
+                <CardContent>
+                  <Typography variant="h5" fontWeight="bold" component="div">
+                    {cuff.cuffName}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    Price: ${cuff.cost} Cuff ID: {cuff.id}
+                  </Typography>
+                  <Typography variant="body2" fontFamily="default">
+                    {cuff.description}
+                  </Typography>
+                  <Typography>
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        collarID={cuff.id}
+                        onClick={(event) => {
+                          var getCuffID = event.target.getAttribute("cuffID");
+                          setChosenCuff(getCuffID);
+                        }}
+                      >
+                        Select me
+                      </Button>
+                    </CardActions>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </div>
+          </card>
+        );
+      })}
+      <Typography variant="h4" color="white">
+        <div>Step Four: Fronts</div>
+      </Typography>
+      {fronts.map((front, index) => {
+        return (
+          <card sx={{ maxWidth: 250 }}>
+            <div key={index}>
+              <ul className="InstaCard">
+                <CardMedia
+                  component="img"
+                  alt="front"
+                  width="250"
+                  image={front.imageOne}
+                />
+                <CardContent>
+                  <Typography variant="h5" fontWeight="bold" component="div">
+                    {front.frontName}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    Price: ${front.cost} Front ID: {front.id}
+                  </Typography>
+                  <Typography variant="body2" fontFamily="default">
+                    {front.description}
+                  </Typography>
+                  <Typography>
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        frontID={front.id}
+                        onClick={(event) => {
+                          var getFrontID = event.target.getAttribute("frontID");
+                          setChosenFront(getFrontID);
+                        }}
+                      >
+                        Select me
+                      </Button>
+                    </CardActions>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </div>
+          </card>
+        );
+      })}
+      <Typography variant="h4" color="white">
+        <div>Step Five: Pockets</div>
+      </Typography>
+      {pockets.map((pocket, index) => {
+        return (
+          <card sx={{ maxWidth: 250 }}>
+            <div key={index}>
+              <ul className="InstaCard">
+                <CardMedia
+                  component="img"
+                  alt="pocket"
+                  width="250"
+                  image={pocket.imageOne}
+                />
+                <CardContent>
+                  <Typography variant="h5" fontWeight="bold" component="div">
+                    {pocket.pocketName}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    Price: ${pocket.cost} Pocket ID: {pocket.id}
+                  </Typography>
+                  <Typography variant="body2" fontFamily="default">
+                    {pocket.description}
+                  </Typography>
+                  <Typography>
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        pocketID={pocket.id}
+                        onClick={(event) => {
+                          var getPocketID =
+                            event.target.getAttribute("pocketID");
+                          setChosenPocket(getPocketID);
+                        }}
+                      >
+                        Select me
+                      </Button>
+                    </CardActions>
+                  </Typography>
+                </CardContent>
+              </ul>
+            </div>
+          </card>
+        );
+      })}
+      <Typography variant="h4" color="white">
+        <div>Step Six: Backs</div>
+      </Typography>
+      {backs.map((back, index) => {
+        return (
+          <card sx={{ maxWidth: 250 }}>
+            <div key={index}>
+              <ul className="InstaCard">
+                <CardMedia
+                  component="img"
+                  alt="back"
+                  width="250"
+                  image={back.imageOne}
+                />
+                <CardContent>
+                  <Typography variant="h5" fontWeight="bold" component="div">
+                    {back.backName}
+                  </Typography>
+                  <Typography variant="body1" component="div">
+                    Price: ${back.cost} Back ID: {back.id}
+                  </Typography>
+                  <Typography variant="body2" fontFamily="default">
+                    {back.description}
+                  </Typography>
+                  <Typography>
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        backID={back.id}
+                        onClick={(event) => {
+                          var getBackID = event.target.getAttribute("backID");
+                          setChosenBack(getBackID);
                         }}
                       >
                         Select me
