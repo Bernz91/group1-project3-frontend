715476e051b395baa26af411af234da8d9931351
Zihao1319 <zihao1319@gmail.com>
2022-11-03 01:43:24 +0800

    changed shopping cart and order summary to auth0, added measurement id component in customisation page

--- src/Component/OrderSummary/InfoModal.js
+++ src/Component/OrderSummary/InfoModal.js
@@ -24,7 +24,7 @@ const InfoModal = (props) => {
   const handleClose = () => setOpen(false);
 
   const item = props.item;
-  console.log(item)
+  console.log(item);
 
   return (
     <div>
@@ -44,103 +44,107 @@ const InfoModal = (props) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" component="h2" sx = {{fontWeight: "bold", mb: 2}}>
+          <Typography
+            id="modal-modal-title"
+            component="h2"
+            sx={{ fontWeight: "bold", mb: 2 }}
+          >
             Your Shirt Design:
           </Typography>
           <Typography
             id="modal-modal-description"
-            sx={{ mt: 2, lineHeight: 1.6, fontFamily: 'Monospace' }}
+            sx={{ mt: 2, lineHeight: 1.6, fontFamily: "Monospace" }}
             variant="overline"
           >
-          <Grid container spacing={2}>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["fabric"].imageOne}
-                alt={item["fabric"].description}
-              />
+            <Grid container spacing={2}>
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["fabric"].imageOne}
+                    alt={item["fabric"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Fabric type: {item["fabric"].fabricName}</Box>
+                </Grid>
               </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Fabric type: {item["fabric"].fabricName}</Box>
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["collar"].imageOne}
+                    alt={item["collar"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Collar type: {item["collar"].collarName}</Box>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["collar"].imageOne}
-                alt={item["collar"].description}
-              />
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["front"].imageOne}
+                    alt={item["front"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Front type: {item["front"].frontName}</Box>
+                </Grid>
               </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Collar type: {item["collar"].collarName}</Box>
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["back"].imageOne}
+                    alt={item["back"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Back type: {item["back"].backName}</Box>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["front"].imageOne}
-                alt={item["front"].description}
-              />
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["cuff"].imageOne}
+                    alt={item["cuff"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Cuff type: {item["cuff"].cuffName}</Box>
+                </Grid>
               </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Front type: {item["front"].frontName}</Box>
+              <Grid item container direction="row" xs={12} sm={6}>
+                <Grid item xs={2}>
+                  <CardMedia
+                    component="img"
+                    sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
+                    image={item["pocket"].imageOne}
+                    alt={item["pocket"].description}
+                  />
+                </Grid>
+                <Grid item xs={10} justify="center">
+                  <Box>Pocket type: {item["pocket"].pocketName}</Box>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["back"].imageOne}
-                alt={item["back"].description}
-              />
-              </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Back type: {item["back"].backName}</Box>
-              </Grid>
-            </Grid>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["cuff"].imageOne}
-                alt={item["cuff"].description}
-              />
-              </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Cuff type: {item["cuff"].cuffName}</Box>
-              </Grid>
-            </Grid>
-            <Grid item container direction="row" xs={12} sm={6}>
-              <Grid item xs={2}>
-                <CardMedia
-                component="img"
-                sx={{ m: 0, p: 0, height: "10vh", width: "10vw" }}
-                image={item["pocket"].imageOne}
-                alt={item["pocket"].description}
-              />
-              </Grid>
-              <Grid item xs={10} justify="center">
-                <Box>Fabric type: {item["pocket"].pocketName}</Box>
-              </Grid>
-            </Grid>
-                
-            {/* <EditShirtDesign item={item}/> */}
-            
-            {/* <Grid> */}
+
+              {/* <EditShirtDesign item={item}/> */}
+
+              {/* <Grid> */}
               {/* <Button  variant="contained" fullWidth
                   sx={{ align: "center", display: "flex", mt: 2, ml: 2}}>
                 Edit design
               </Button> */}
-            {/* </Grid> */}
-          </Grid>
+              {/* </Grid> */}
+            </Grid>
           </Typography>
         </Box>
       </Modal>
